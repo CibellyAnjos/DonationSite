effb26b3a63b9f4078d5b9d490e48c7a9475d638
Victor <victor-cosme@hotmail.com>
2021-08-01 20:13:46 -0300

    Primeira div
    Primeira div para o formulário, dentro dela ficará o formulário

--- formcadastro.php
+++ formcadastro.php
@@ -1,7 +1,20 @@
 <?php
 
-
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+
+    </title>
+</head>
+
+<body>
+    <div id="caixaform" style="border-style: solid; border-color: black; width: 50%; height: 300px; padding-left: 50px; margin: 0 auto;">
+        Boa tarde
+    </div>
+</body>
+
+</html>
