44bdb840f31126da25e62d2b1687938eb127b82e
Victor <victor-cosme@hotmail.com>
2021-08-01 21:02:32 -0300

    Primeiro campo do form (nome)

--- formcadastro.php
+++ formcadastro.php
@@ -13,7 +13,16 @@
 
 <body>
     <div id="caixaform" style="border-style: solid; border-color: black; width: 50%; height: 300px; padding-left: 50px; margin: 0 auto;">
-        Boa tarde
+        <font face="Antapani">    
+            <h1>Cadastro</h1>
+        </font>
+        <br/>
+        <font face="Montserrat">
+            <div>
+                <label for="">Nome: </label>
+                <input name="nome" required="required" placeholder="Seu nome" type="text">
+            </div>
+        </font>
     </div>
 </body>
 
